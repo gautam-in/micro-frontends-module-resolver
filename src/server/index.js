8399f37919194834a590ab2722603d9e1503bbfb
Vidit Shah <viditisonline@gmail.com>
2018-09-14 14:05:30 +0530

    Added TODO app for demo

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ import fetch from "./fetch";
 import requireFromString from "require-from-string";
 
 const urlPrefix = "http://localhost:8002/shared";
+const API_SERVER = "http://localhost:8003";
 
 const app = express();
 
@@ -34,12 +35,27 @@ app.use(async (req, res, next) => {
   );
 
   const configureStoreSource = await fetch(urlPrefix + "/configureStore.js");
+  const globalReducerSource = await fetch(urlPrefix + "/globalReducer.js");
 
   const moduleObj = requireFromString(moduleSource);
   const configureStore = requireFromString(configureStoreSource).default;
+  const globalReducer = requireFromString(globalReducerSource).default;
+
+  // TODO: Gather initials task from API server
+  // const globalActionTypes = requireFromString(globalReducerSource).ActionTypes;
+  // const globalData = await fetch(API_SERVER + "/global", undefined, true);
+  // console.log(globalData);
 
   req.store = configureStore();
-  req.store.attachReducers({ [moduleName]: moduleObj.rootReducer });
+  if (moduleObj.rootReducer)
+    req.store.attachReducers({ [moduleName]: moduleObj.rootReducer });
+
+  req.store.attachReducers({ globalData: globalReducer });
+
+  // req.store.dispatch({
+  //   type: globalActionTypes.INIT_GLOBAL_DATA,
+  //   state: globalData
+  // });
 
   req.moduleObj = moduleObj;
   req.moduleManifest = moduleManifest;
